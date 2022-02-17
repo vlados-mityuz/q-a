6bd2ebee32b3b8ebb8229c20c6d40465784ccf79
Vladislav <thenoma1@gmail.com>
2022-02-17 11:35:06 +0300

    Add flash messages template

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  FLASH_TYPES_TO_CSS_CLASS = {
+    notice: 'info',
+    alert: 'warning',
+    error: 'danger'
+  }.freeze
+
+  def class_for_flash_message(key)
+    FLASH_TYPES_TO_CSS_CLASS.fetch(key.to_sym, key)
+  end
 end
