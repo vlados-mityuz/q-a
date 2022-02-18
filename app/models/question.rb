4a7cde3c06239b85d5119f7deb0623d1f60698a3
Vladislav <thenoma1@gmail.com>
2022-02-18 14:03:31 +0300

    User can attach files while creating question

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,6 +3,8 @@ class Question < ApplicationRecord
   belongs_to :author, class_name: "User"
   belongs_to :best_answer, class_name: 'Answer', optional: true
 
+  has_many_attached :files
+
   validates :title, :body, presence: true
 
   def other_answers
