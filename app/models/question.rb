2aa195f58ab08110fa889e669888d8e7d1c4f235
Vladislav <thenoma1@gmail.com>
2022-02-18 12:24:25 +0300

    Add other_answers method to question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,4 +4,8 @@ class Question < ApplicationRecord
   belongs_to :best_answer, class_name: 'Answer', optional: true
 
   validates :title, :body, presence: true
+
+  def other_answers
+    self.answers.where.not(id: self.best_answer_id)
+  end
 end
